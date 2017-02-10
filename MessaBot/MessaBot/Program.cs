aad0b383d66685517bf1eab388c203f0bff76b53
Sadik Messaoudi <sadikano10@gmail.com>
2017-02-10 18:40:35 +0100

    Ajout de 3 commandes Remove/Ajout/Travail

--- MessaBot/MessaBot/Program.cs
+++ MessaBot/MessaBot/Program.cs
@@ -11,6 +11,7 @@ using RiotSharp.StaticDataEndpoint;
 using RiotSharp.CurrentGameEndpoint;
 using RiotSharp.SummonerEndpoint;
 using RiotSharp.StatsEndpoint;
+using System.Timers;
 
 namespace MessaBot
 {
@@ -19,6 +20,9 @@ namespace MessaBot
 
     static void Main(string[] args) => new Program().Run(args).GetAwaiter().GetResult();
         RiotApi api;
+        List<string> user = new List<String>(){ "MG Shiglet", "Messa Riven", "GrosZboubTMN" };
+        bool fini = false;
+        List<long> matchIDs = new List<long>();
         StaticRiotApi staticApi;
         DiscordClient client;
         List<ChampionStatic> champions;
@@ -85,17 +89,17 @@ namespace MessaBot
             {
                 try
                 {
-                    Console.WriteLine("Recuperation des ranked stats par son id pour "+sum[i].Name);
+                    //Console.WriteLine("Recuperation des ranked stats par son id pour "+sum[i].Name);
                     var stats = api.GetStatsRanked(Region.euw, sum.Find(c=>c.Name == item.SummonerName).Id);
-                    Console.WriteLine(item.SummonerName+" =? " + sum[i].Name);
-                    Console.WriteLine("Fin de recup des ranked stats par son id");
+                    //Console.WriteLine(item.SummonerName+" =? " + sum[i].Name);
+                    //Console.WriteLine("Fin de recup des ranked stats par son id");
                     var x = stats.Find(c => c.ChampionId == item.ChampionId);
                     var n = x.Stats.TotalSessionsPlayed;
                     var k = (x.Stats.TotalChampionKills / n).ToString();
                     var d = (x.Stats.TotalDeathsPerSession / n).ToString();
                     var a = (x.Stats.TotalAssists / n).ToString();
                     var st =  k+ "/" + d+ "/" + a;
-                    Console.WriteLine("Pour {0} games : "+st,n);
+                    //Console.WriteLine("Pour {0} games : "+st,n);
                     l.Add(st);
                 }
                 catch (RiotSharpException)
@@ -116,8 +120,124 @@ namespace MessaBot
             Summoner x = api.GetSummoner(Region.euw, name);
             return x;
         }
+        public async Task job()
+        {
+            string reponse = "Pas en game";
+            for (int j = 0; j < user.Count; j++)
+            {
+                Console.WriteLine("Recherche de " + user[j] + ".");
+                try
+                {
+                    var summoner = api.GetSummoner(Region.euw, user[j]);
+                    var match = api.GetCurrentGame(Platform.EUW1, summoner.Id);
+                    if (matchIDs.Contains(match.GameId))
+                        throw new Exception();
+                    matchIDs.Add(match.GameId);
+                    var participants = match.Participants;
+                    List<string> sumId = new List<string>();
+                    List<Summoner> sum = new List<Summoner>();
+                    foreach (var item in participants)
+                    {
+                        sumId.Add(item.SummonerName);
+                    }
+                    sum = api.GetSummoners(Region.euw, sumId);
+                    //Console.WriteLine("Début du formattage du KDA");
+                    var lStats = statsByParticipants(participants, sum);
+                    Console.WriteLine("Fin du formattage du KDA");
+                    var bans = match.BannedChampions;
+                    bans.Sort(delegate (BannedChampion x, BannedChampion y)
+                    {
+                        return x.TeamId.CompareTo(y.TeamId);
+                    });
+                    string[] ban = { championByID(bans[0].ChampionId), championByID(bans[1].ChampionId), championByID(bans[2].ChampionId), championByID(bans[3].ChampionId), championByID(bans[4].ChampionId), championByID(bans[5].ChampionId) };
+
+                    reponse = "```Red Team-- Bans[" + ban[0] + ", " + ban[1] + ", " + ban[2] + "]" + Environment.NewLine + Environment.NewLine;
+                    //Console.WriteLine("Debut de recup du rang");
+                    var l = rangBySummoner(participants, sum);
+                    Console.WriteLine("Fin de récup du rang");
+                    int i = 0;
+                    IEnumerable<Tuple<string, string, string, string, string, string>> authors =
+                    new[]
+                    {
+                        Tuple.Create(participants[0].SummonerName,l[0] , championByID(participants[0].ChampionId),lStats[i],spellByID(participants[0].SummonuerSpell1),spellByID(participants[0].SummonerSpell2)),
+                        Tuple.Create(participants[1].SummonerName,l[1] , championByID(participants[1].ChampionId),lStats[i+1],spellByID(participants[1].SummonuerSpell1),spellByID(participants[1].SummonerSpell2)),
+                        Tuple.Create(participants[2].SummonerName,l[2] , championByID(participants[2].ChampionId),lStats[i+2],spellByID(participants[2].SummonuerSpell1),spellByID(participants[2].SummonerSpell2)),
+                        Tuple.Create(participants[3].SummonerName,l[3] , championByID(participants[3].ChampionId),lStats[i+3],spellByID(participants[3].SummonuerSpell1),spellByID(participants[3].SummonerSpell2)),
+                        Tuple.Create(participants[4].SummonerName,l[4] , championByID(participants[4].ChampionId),lStats[i+4],spellByID(participants[4].SummonuerSpell1),spellByID(participants[4].SummonerSpell2))
+                    };
+
+                    reponse += authors.ToStringTable(
+                    new[] { "Pseudo", "Rang", "Champion", "KDA", "Spell 1", "Spell 2" },
+                    a => a.Item1, a => a.Item2, a => a.Item3, a => a.Item4, a => a.Item5, a => a.Item6) + Environment.NewLine + Environment.NewLine;
+                    reponse += "Blue Team-- Bans[" + ban[3] + ", " + ban[4] + ", " + ban[5] + "]" + Environment.NewLine + Environment.NewLine;
+                    i = 5;
+                    authors =
+                    new[]
+                    {
+                        Tuple.Create(participants[5].SummonerName,l[i+0] , championByID(participants[5].ChampionId),lStats[i],spellByID(participants[5].SummonuerSpell1),spellByID(participants[5].SummonerSpell2)),
+                        Tuple.Create(participants[6].SummonerName,l[i+1] , championByID(participants[6].ChampionId),lStats[i+1],spellByID(participants[6].SummonuerSpell1),spellByID(participants[6].SummonerSpell2)),
+                        Tuple.Create(participants[7].SummonerName,l[i+2] , championByID(participants[7].ChampionId),lStats[i+2],spellByID(participants[7].SummonuerSpell1),spellByID(participants[7].SummonerSpell2)),
+                        Tuple.Create(participants[8].SummonerName,l[i+3] , championByID(participants[8].ChampionId),lStats[i+3],spellByID(participants[8].SummonuerSpell1),spellByID(participants[8].SummonerSpell2)),
+                        Tuple.Create(participants[9].SummonerName,l[i+4] , championByID(participants[9].ChampionId),lStats[i+4],spellByID(participants[9].SummonuerSpell1),spellByID(participants[9].SummonerSpell2))
+                    };
+                    reponse += authors.ToStringTable(
+                    new[] { "Pseudo", "Rang", "Champion", "KDA", "Spell 1", "Spell 2" },
+                    a => a.Item1, a => a.Item2, a => a.Item3, a => a.Item4, a => a.Item5, a => a.Item6) + Environment.NewLine + "```";
+                    var channel = await client.GetChannelByID(255746001724243968);
+                    await client.SendMessage(channel, reponse);
+                }
+                catch (RiotSharpException)
+                {
+                    reponse = "Pas en game";
+                    Console.WriteLine(reponse);
+                }
+                catch (Exception)
+                {
+                    reponse = "Même game !!";
+                    Console.WriteLine(reponse);
+                }
+            }
+        }
         void CreateCommands(DiscordClient _client)
         {
+
+            _client.AddCommand("Travail", async (e) =>
+           {
+               await e.Message.Parent.SendMessage("Wakhaaa a ouma inô!");
+               Console.WriteLine("Command  Reveille toi");
+               while (!fini)
+               {
+                   var watch = System.Diagnostics.Stopwatch.StartNew();
+                   await job();
+                   watch.Stop();
+                   int t = (int)(30000 - watch.ElapsedMilliseconds);
+                   if (watch.ElapsedMilliseconds < 25000)
+                   {
+                       Console.WriteLine("Attende de " + t + " ms");
+                       await Task.Delay(t);
+                   }
+                   else Console.WriteLine("Pas d'attente");
+               }
+           });
+            _client.AddCommand("Ajoute", async(e) =>
+            {
+                string reponse = "Joueur ajouté.";
+                string ajout = e.Message.Content.Split(new string[] { "$Ajoute " }, StringSplitOptions.RemoveEmptyEntries)[0];
+                Console.WriteLine("Command Ajoute");
+                if (user.Contains(ajout))
+                    reponse = "Je l'espionne déjà.";
+                else user.Add(ajout);
+                await e.Message.Parent.SendMessage(reponse);
+            });
+            _client.AddCommand("Remove", async (e) =>
+            {
+                string reponse = "C'est fait !";
+                string ajout = e.Message.Content.Split(new string[] { "$Remove " }, StringSplitOptions.RemoveEmptyEntries)[0];
+                Console.WriteLine("Command Remove");
+                if (user.Contains(ajout))
+                    user.Remove(ajout);
+                await e.Message.Parent.SendMessage(reponse);
+            });
             _client.AddCommand("Game", async (e) =>
             {
                 var watch = System.Diagnostics.Stopwatch.StartNew();
@@ -170,11 +290,11 @@ namespace MessaBot
                     authors =
                     new[]
                     {
-                        Tuple.Create(participants[5].SummonerName,l[i+0] , championByID(participants[5].ChampionId),"0/0/0",spellByID(participants[5].SummonuerSpell1),spellByID(participants[5].SummonerSpell2)),
-                        Tuple.Create(participants[6].SummonerName,l[i+1] , championByID(participants[6].ChampionId),"0/0/0",spellByID(participants[6].SummonuerSpell1),spellByID(participants[6].SummonerSpell2)),
-                        Tuple.Create(participants[7].SummonerName,l[i+2] , championByID(participants[7].ChampionId),"0/0/0",spellByID(participants[7].SummonuerSpell1),spellByID(participants[7].SummonerSpell2)),
-                        Tuple.Create(participants[8].SummonerName,l[i+3] , championByID(participants[8].ChampionId),"0/0/0",spellByID(participants[8].SummonuerSpell1),spellByID(participants[8].SummonerSpell2)),
-                        Tuple.Create(participants[9].SummonerName,l[i+4] , championByID(participants[9].ChampionId),"0/0/0",spellByID(participants[9].SummonuerSpell1),spellByID(participants[9].SummonerSpell2))
+                        Tuple.Create(participants[5].SummonerName,l[i+0] , championByID(participants[5].ChampionId),lStats[i],spellByID(participants[5].SummonuerSpell1),spellByID(participants[5].SummonerSpell2)),
+                        Tuple.Create(participants[6].SummonerName,l[i+1] , championByID(participants[6].ChampionId),lStats[i+1],spellByID(participants[6].SummonuerSpell1),spellByID(participants[6].SummonerSpell2)),
+                        Tuple.Create(participants[7].SummonerName,l[i+2] , championByID(participants[7].ChampionId),lStats[i+2],spellByID(participants[7].SummonuerSpell1),spellByID(participants[7].SummonerSpell2)),
+                        Tuple.Create(participants[8].SummonerName,l[i+3] , championByID(participants[8].ChampionId),lStats[i+3],spellByID(participants[8].SummonuerSpell1),spellByID(participants[8].SummonerSpell2)),
+                        Tuple.Create(participants[9].SummonerName,l[i+4] , championByID(participants[9].ChampionId),lStats[i+4],spellByID(participants[9].SummonuerSpell1),spellByID(participants[9].SummonerSpell2))
                     };
                     reponse += authors.ToStringTable(
                     new[] { "Pseudo", "Rang", "Champion", "KDA", "Spell 1", "Spell 2" },
